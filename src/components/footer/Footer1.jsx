39d33e6424d2639df15251b14002c8bd410c4e77
Mario Zito <mario.zito@treme.io>
2024-10-04 00:04:39 -0300

    Fix footer (wip)

--- src/components/footer/Footer1.jsx
+++ src/components/footer/Footer1.jsx
@@ -9,8 +9,8 @@ export default function Footer1() {
   return (
     <footer className="page-footer bg-white dark:bg-jacarta-900">
       <div className="container">
-        <div className="grid grid-cols-6 gap-x-7 gap-y-14 pt-24 pb-12 md:grid-cols-12">
-          <div className="col-span-full sm:col-span-3 md:col-span-4">
+        <div className="grid grid-cols-3 gap-x-7 gap-y-14 pt-24 pb-12 md:grid-cols-12">
+          <div className="col-span-full sm:col-span-3 md:col-span-8 pe-96">
             <Link href="/" className="mb-6 block flex items-center">
               <span class="inline-block">
                 <Image
@@ -44,14 +44,14 @@ export default function Footer1() {
             </div>
           </div>
 
-          <div className="col-span-full sm:col-span-3 md:col-span-2 md:col-start-7">
+          {/* <div className="col-span-full sm:col-span-3 md:col-span-2 md:col-start-7">
             <h3 className="mb-6 font-display text-sm text-jacarta-700 dark:text-white">
               Marketplace
             </h3>
             <ul className="flex flex-col space-y-1 dark:text-jacarta-300">
               <MarketplaceLinks />
             </ul>
-          </div>
+          </div> */}
 
           <div className="col-span-full sm:col-span-3 md:col-span-2">
             <h3 className="mb-6 font-display text-sm text-jacarta-700 dark:text-white">
@@ -71,6 +71,7 @@ export default function Footer1() {
             </ul>
           </div>
         </div>
+
         <div className="flex flex-col items-center justify-between space-y-2 py-8 sm:flex-row sm:space-y-0">
           <span className="text-sm dark:text-jacarta-400">
             &copy; {new Date().getFullYear()} Minatokens — Made by{" "}
