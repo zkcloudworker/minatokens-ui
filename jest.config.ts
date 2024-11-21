9b82dac9a23865cd73633dc36312b796af787279
DFST <mk@dfst.io>
2024-11-22 02:30:35 +0300

    test

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  // silent: false,
+  // verbose: false,
+};
+
+export default config;
