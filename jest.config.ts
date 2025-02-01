85db687171e4261818113e0f9a9ff987b3c34770
DFST <mk@dfst.io>
2025-02-01 16:04:16 +0300

    arweave

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,11 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testMatch: ["**/test/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
